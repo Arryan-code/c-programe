16b4e2cc28c5ae608f328f432a6935f8cfdd5f44
Arryan-code <arryan.busy@gmail.com>
2024-10-16 14:10:49 +0530

    Update arryan.c

--- arryan.c
+++ arryan.c
@@ -6,29 +6,29 @@ int main (){
 
     switch (days)
     {
-    case 1 :printf(" JANUARY : DAYS 31");
+    case 1 :printf(" JANUARY : DAYS 31\n");
         break;
-    case 2 :printf(" FEBUARY : DAYS 28");
+    case 2 :printf(" FEBUARY : DAYS 28\n");
         break;
-    case 3 :printf(" MARCH : DAYS 31");
+    case 3 :printf(" MARCH : DAYS 31\n");
         break;
-    case 4 :printf(" APRIL : DAYS 30");
+    case 4 :printf(" APRIL : DAYS 30\n");
         break;
-    case 5 :printf(" MAY : DAYS 31");
+    case 5 :printf(" MAY : DAYS 31\n");
         break;
-    case 6 :printf(" JUNE : DAYS 30");
+    case 6 :printf(" JUNE : DAYS 30\n");
         break;
-    case 7 :printf(" JULY : DAYS 31");
+    case 7 :printf(" JULY : DAYS 31\n");
         break;
-    case 8 :printf(" AUGUST : DAYS 31");
+    case 8 :printf(" AUGUST : DAYS 31\n");
         break;
-    case 9 :printf(" SEPTEMBER : DAYS 30");
+    case 9 :printf(" SEPTEMBER : DAYS 30\n");
         break;
-    case 10 :printf("OCTOBER  : DAYS 31");
+    case 10 :printf("OCTOBER  : DAYS 31\n");
         break;
-    case 11:printf(" NOVEMBER  : DAYS 30");
+    case 11:printf(" NOVEMBER  : DAYS 30\n");
         break;
-    case 12:printf(" DECEMBER : DAYS 31");
+    case 12:printf(" DECEMBER : DAYS 31\n");
         break;
     
     default: printf(" not a valid !");
